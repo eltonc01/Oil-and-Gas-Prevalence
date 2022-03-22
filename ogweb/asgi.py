86ea66791197983be5948920f8e40bb037253273
Elton Cao <63972282+eltonc01@users.noreply.github.com>
2022-03-21 19:41:10 -0600

    Add files via upload

---
+++ ogweb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ogweb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ogweb.settings')
+
+application = get_asgi_application()
